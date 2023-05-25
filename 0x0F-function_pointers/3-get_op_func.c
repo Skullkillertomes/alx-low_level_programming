9deebfa14e9916a2eb2191c49a66965a55d49f21
Skullkillertomes <akandeoluwatomisin41@gmail.com>
2023-05-25 15:37:32 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,6 +12,7 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
+
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -20,6 +21,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL},
 	};
 	int i = 0;
+
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 	return (ops[i].f);
