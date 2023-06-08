a63037fb1cd820a817d6541a6c26ba4868398dd7
Skullkillertomes <akandeoluwatomisin41@gmail.com>
2023-06-08 16:32:18 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sts the value to 0
+ * @n: pointer to the number
+ * @index: index to clear
+ *
+ * Return: 1 for success, -1 for failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
